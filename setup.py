8ee19b6ae290a5220a48347c3c05222b90b06667
Kevin Nogueira <kevinnog@outlook.com.br>
2021-02-19 11:25:40 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='chatbot_v1',
+    packages=['chatbot_v1'],
+    description='Hello world enterprise edition',
+    version='0.1',
+    url='http://github.com/kevinnog/chatbot_v1',
+    author='Kevin',
+    author_email='kevin@brain.agr.br',
+    keywords=['pip','brain']
+    )
