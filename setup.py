c1f6f9febfd26d7809c13e21362c49895af0ac7c
Kevin Nogueira <kevinnog@outlook.com.br>
2021-02-19 11:31:59 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-kevinnog", # Replace with your own username
+    name="chatbot",
     version="0.0.1",
     author="Kevin",
     author_email="kevin@brain.agr.br",
