e208658cb68704bf71cdf32aa6239c815907cbe1
Kevin Nogueira <kevinnog@outlook.com.br>
2021-02-19 11:28:58 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='chatbot_v1',
-    packages=['chatbot_v1'],
-    description='Hello world enterprise edition',
-    version='0.1',
-    url='http://github.com/kevinnog/chatbot_v1',
-    author='Kevin',
-    author_email='kevin@brain.agr.br',
-    keywords=['pip','brain']
-    )
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-kevinnog", # Replace with your own username
+    version="0.0.1",
+    author="Kevin",
+    author_email="kevin@brain.agr.br",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kevinnog/chatbot_v1",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
